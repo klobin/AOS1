d3340ce8664b0462f8ec942b63026c7106520c0e
pthakare <pranayjeet.thakare@hitachivantara.com>
2019-10-14 17:16:43 -0700

    refractor and added FCFS

--- https:/github.com/klobin/AOS1.git/ABC/src/FCFS.java
+++ https:/github.com/klobin/AOS1.git/ABC/src/FCFS.java
@@ -1,91 +1,49 @@
+import java.util.List;
 
-public class FCFS{ 
+public class FCFS {
 
+    /**
+     * Driver for FCFS
+     *
+     * @param processes
+     */
+    public Stat run(List<Process> processes) {
+        int totalTurnAroundTime = 0;
+        int totalWaitingTime = 0;
+        int totalResponseTime = 0;
 
-// Function to find the waiting time for all 
-// processes 
-static void findWaitingTime(int processes[], int n, int bt[], int wt[], int at[]) 
-{ 
-	int service_time[] = new int[n]; 
-	service_time[0] = 0; 
-	wt[0] = 0; 
+        StringBuilder processesRunning = new StringBuilder();
+        System.out.println("\n\nRunning First Come First Serve");
+        System.out.println("Name" + "\t" + "Arrival Time" + "\t" + "Run Time" + "\t" + "Priority" + "\t" + "Turn Around Time" + "\t" + "Waiting Time" + "\t" + "Response Time");
+        processes.get(0).setWaitingTime(0);
+        processes.get(0).setResponseTime(0);
+        processes.get(0).setTurnAroundTime(processes.get(0).getRunTime());
+        for (int i = 1; i < processes.size(); i++) {
+            processesRunning.append(processes.get(i).getName());
 
-	// calculating waiting time 
-	for (int i = 1; i < n ; i++) 
-	{ 
-		// Add burst time of previous processes 
-		service_time[i] = service_time[i-1] + bt[i-1]; 
+            processes.get(i).setResponseTime(processes.get(i - 1).getResponseTime() + processes.get(i - 1).getRunTime());
+            int wt = processes.get(i - 1).getRunTime() + processes.get(i - 1).getArrivalTime() + processes.get(i - 1).getWaitingTime();
+            wt = wt - processes.get(i).getArrivalTime();
+            if (wt < 0) {
+                wt = 0;
+            }
+            processes.get(i).setWaitingTime(wt);
 
-		// Find waiting time for current process = 
-		// sum - at[i] 
-		wt[i] = service_time[i] - at[i]; 
+            processes.get(i).setTurnAroundTime(processes.get(i).getWaitingTime() + processes.get(i).getRunTime());
 
-		// If waiting time for a process is in negative 
-		// that means it is already in the ready queue 
-		// before CPU becomes idle so its waiting time is 0 
-		if (wt[i] < 0) 
-			wt[i] = 0; 
-	} 
-} 
+            totalResponseTime = totalResponseTime + processes.get(i).getResponseTime();
+            totalWaitingTime = totalWaitingTime + processes.get(i).getWaitingTime();
+            totalTurnAroundTime = totalTurnAroundTime + processes.get(i).getTurnAroundTime();
+        }
 
-// Function to calculate turn around time 
-static void findTurnAroundTime(int processes[], int n, int bt[], 
-									int wt[], int tat[]) 
-{ 
-	// Calculating turnaround time by adding bt[i] + wt[i] 
-	for (int i = 0; i < n ; i++) 
-		tat[i] = bt[i] + wt[i]; 
-} 
+        for (Process p : processes) {
+            System.out.println(p.toString());
+        }
 
-// Function to calculate average waiting and turn-around 
-// times. 
-static void findavgTime(int processes[], int n, int bt[], int at[]) 
-{ 
-	int wt[] = new int[n], tat[] = new int[n]; 
-
-	// Function to find waiting time of all processes 
-	findWaitingTime(processes, n, bt, wt, at); 
-
-	// Function to find turn around time for all processes 
-	findTurnAroundTime(processes, n, bt, wt, tat); 
-
-	// Display processes along with all details 
-	System.out.print("Processes " + " Burst Time " + " Arrival Time "
-		+ " Waiting Time " + " Turn-Around Time "
-		+ " Completion Time \n"); 
-	int total_wt = 0, total_tat = 0; 
-	for (int i = 0 ; i < n ; i++) 
-	{ 
-		total_wt = total_wt + wt[i]; 
-		total_tat = total_tat + tat[i]; 
-		int compl_time = tat[i] + at[i]; 
-		System.out.println(i+1 + "\t\t" + bt[i] + "\t\t"
-			+ at[i] + "\t\t" + wt[i] + "\t\t "
-			+ tat[i] + "\t\t " + compl_time); 
-	} 
-
-	System.out.print("Average waiting time = "
-		+ (float)total_wt / (float)n); 
-	System.out.print("\nAverage turn around time = "
-		+ (float)total_tat / (float)n); 
-} 
-
-// Driver code 
-
-	public static void main(String args[]) { 
-		// Process id's 
-	int processes[] = {1, 2, 3}; 
-	int n = processes.length; 
-
-	// Burst time of all processes 
-	int burst_time[] = {5, 9, 6}; 
-
-	// Arrival time of all processes 
-	int arrival_time[] = {0, 3, 6}; 
-
-	findavgTime(processes, n, burst_time, arrival_time); 
-
-	} 
-} 
-
-/*This code is contributed by PrinciRaj1992*/
+        System.out.println("Processes Ran: " + processesRunning.toString());
+        System.out.println("\nAverage Turnaround Time: " + ((float) totalTurnAroundTime / (float) processes.size()));
+        System.out.println("Average Waiting Time: " + ((float) totalWaitingTime / (float) processes.size()));
+        System.out.println("Average Response Time: " + ((float) totalResponseTime / (float) processes.size()));
+        return new Stat((float) totalTurnAroundTime / (float) processes.size(), (float) totalResponseTime / (float) processes.size(), (float) totalWaitingTime / (float) processes.size());
+    }
+}
--- https:/github.com/klobin/AOS1.git/ABC/src/ScheduleAlgo.java
+++ https:/github.com/klobin/AOS1.git/ABC/src/ScheduleAlgo.java
@@ -1,21 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 public class ScheduleAlgo {
-	public static void main(String[] args) {
-		List<Process> inputs = new ArrayList<>();
-		for (int i = 0; i < 10; i++) {
-			inputs.add(getRandomProcess());
-		}
-		inputs.stream().sorted().forEach(i -> System.out.println(i.arrivalTime + " " + i.runTime + " " + i.priority));
-	}
+    public static void main(String[] args) {
+        FCFS fcfc = new FCFS();
+        float turnaroundTime = 0;
+        float responseTime = 0;
+        float waitingTime = 0;
 
-	private static Process getRandomProcess() {
-		return new Process(generateRandomInRange(0, 99), generateRandomInRange(1, 10), generateRandomInRange(1, 4));
-	}
+        for (int i = 0; i < 5; i++) {
+            Process.setCh('A');
+            Stat s = fcfc.run(Process.generateProcesses(10));
+            turnaroundTime = turnaroundTime + s.getTurnAroudTime();
+            responseTime = responseTime + s.getResponseTime();
+            waitingTime = waitingTime + s.getWaitingTIme();
+        }
 
-	private static int generateRandomInRange(int min, int max) {
-		return new Random().nextInt((max - min) + 1) + min;
-	}
+        System.out.println("\n\nCombined Stats for FCFS over 5 sets of input:");
+        System.out.println("Turnaround Time: "+ turnaroundTime/5);
+        System.out.println("Response Time: "+ responseTime/5);
+        System.out.println("Waiting Time: "+ waitingTime/5);
+
+    }
 }
